4fa08aa1ab60ed8ac56b1defbdb1026202a2ff41
Anuoluwa Adebayo <adebayo.anuoluwa02@gmail.com>
2023-12-15 22:40:57 +0100

    category carousel: initial build config

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,7 +1,6 @@
 import "../../styles/css/footer.css";
 
 function Footer({ footerState }) {
-  footerState = false;
   const year = new Date().getFullYear();
   const footer = footerState ? (
     <FooterShort date={year} />
