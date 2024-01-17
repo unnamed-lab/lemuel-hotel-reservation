0339e4a444a382b3e31eaa491fe9abe4e968d031
Anuoluwa Adebayo <adebayo.anuoluwa02@gmail.com>
2024-01-17 15:59:59 +0100

    carousel navigation section

---
+++ src/utils/hotelcategory.js
@@ -0,0 +1,62 @@
+"use strict";
+
+export const hotelCategory = {
+  resort: {
+    title: "Resort Hotels",
+    tags: ["resort", "tourist", "spa", "retreat", "hangout", "hotel"],
+  },
+  suite: {
+    title: "Suite Hotels",
+    tags: ["suite", "hotel", "court", "cortege", "chambers"],
+  },
+  extended_stay: {
+    title: "Extended Stay Hotels",
+    tags: ["extended", "stay", "hotel", "residence", "visit"],
+  },
+  home: {
+    title: "Homestays",
+    tags: ["home", "stay", "homestay", "sojourn", "layover", "visit"],
+  },
+  vacation: {
+    title: "Vacation",
+    tags: ["vacation", "tour", "stay", "visit", "holiday"],
+  },
+  conference: {
+    title: "Conference Centers",
+    tags: [
+      "conference",
+      "centers",
+      "forum",
+      "seminar",
+      "congress",
+      "meeting",
+      "convention",
+      "colloquium",
+    ],
+  },
+  guest: {
+    title: "Guest House",
+    tags: ["guest", "house", "motel", "boarding", "inn", "hostelry"],
+  },
+  city: {
+    title: "City Hotel",
+    tags: ["city", "hotel", "big", "house", "icebox"],
+  },
+  motel: {
+    title: "Motel",
+    tags: [
+      "motel",
+      "spa",
+      "resort",
+      "lodging",
+      "accomodation",
+      "campground",
+      "hostel",
+      "lodge",
+    ],
+  },
+  casino: {
+    title: "Casino Hotels",
+    tags: ["casino", "hotel"],
+  },
+};
